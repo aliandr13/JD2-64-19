38bfbba94b4dc378722f627128aaf94b4cf3e6d1
Roman <All.Under.Control@mail.ru>
2019-12-05 01:20:12 +0300

    Add Hello_Servlet

--- cars-offer-parrent/cars-offer-web/src/main/Java/by/it/academy/offer/HelloServlet.java
+++ cars-offer-parrent/cars-offer-web/src/main/Java/by/it/academy/offer/HelloServlet.java
@@ -1,7 +1,20 @@
 package by.it.academy.offer;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h2>Hello it Academy!<h2>");
+    }
 
 }
---
+++ cars-offer-parrent/cars-offer-web/src/main/Java/by/it/academy/offer/NameServlet.java
@@ -0,0 +1,11 @@
+package by.it.academy.offer;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+
+@WebServlet(name = "NameServlet", urlPatterns = "/saySomething")
+public class NameServlet extends HttpServlet {
+
+}

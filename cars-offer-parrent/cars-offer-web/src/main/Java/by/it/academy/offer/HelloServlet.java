d269a543bb80df6539b8060eb494c7ff21a52681
Roman <All.Under.Control@mail.ru>
2019-12-05 15:27:32 +0300

    Add Hello_Servlet.1

--- cars-offer-parrent/cars-offer-web/src/main/Java/by/it/academy/offer/HelloServlet.java
+++ cars-offer-parrent/cars-offer-web/src/main/Java/by/it/academy/offer/HelloServlet.java
@@ -12,6 +12,7 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req,resp);
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println("<h2>Hello it Academy!<h2>");
--- cars-offer-parrent/cars-offer-web/src/main/Java/by/it/academy/offer/NameServlet.java
+++ cars-offer-parrent/cars-offer-web/src/main/Java/by/it/academy/offer/NameServlet.java
@@ -1,11 +1,21 @@
 package by.it.academy.offer;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet(name = "NameServlet", urlPatterns = "/saySomething")
 public class NameServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h2>Hello it Academy!!!<h2>");
+    }
 }
